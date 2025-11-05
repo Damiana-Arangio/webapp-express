10e9365199d70fb54db95f0c5d75f3c175c35d98
Damiana-Arangio <arangio.damiana@gmail.com>
2025-11-05 15:00:13 +0100

    Milestone 3 (repo webapp-react) Aggiunta del middleware CORS: - Installato e configurato il middleware CORS in app.js per abilitare le richieste provenienti dal frontend React (repo WebApp-React) in esecuzione su http://localhost:5173. - Rimossa la rotta di test da app.js.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
     IMPORT
 ************/
 const express = require('express')                           // Import del modulo Express
+const cors = require('cors');                                // Import del middleware cors
 const chalk = require('chalk');                              // Import del pacchetto chalk
 const moviesRouter = require('./routers/moviesRouter');      // Import del router che gestisce le rotte dei film
 const errorServer = require('./middlewares/errorServer');    // Import del middleware errorServer
@@ -18,23 +19,15 @@ const app = express();           // Inizializzazione dell'app Express
 const port = 3000;               // Definizione della porta su cui il server deve rimanere in ascolto
 
 
-/************
-    ROTTE
-************/
-app.get('/api', (req, res) => {
-    console.log("Rotta home API attiva");
-    res.send("Benvenuto nella home della API per i film!");
-})
-
-
 /***************
     MIDDLEWARE
 ****************/
-app.use(imagePath);                      // Registrazione del middleware per gestire dinamicamente il path delle immagini
-app.use(express.static('public'));       // Registrazione del middleware per rendere accessibili i file statici (es. immagini) contenuti nella cartella "public"
-app.use('/api/movies', moviesRouter);    // Registrazione del router con prefisso /api 
-app.use(errorServer);                    // Registrazione del middleware "errorServer" che gestisce gli errori interni del server 
-app.use(notFound);                       // Registrazione del middleware "notFound" che gestisce le rotte inesistenti (404 Not Found)
+app.use(cors({ origin: 'http://localhost:5173' }))      // Registrazione del middleware cors per consentire al server di specificare quali origini possono accedere alle sue risorse
+app.use(imagePath);                                     // Registrazione del middleware per gestire dinamicamente il path delle immagini
+app.use(express.static('public'));                      // Registrazione del middleware per rendere accessibili i file statici (es. immagini) contenuti nella cartella "public"
+app.use('/api/movies', moviesRouter);                   // Registrazione del router con prefisso /api 
+app.use(errorServer);                                   // Registrazione del middleware "errorServer" che gestisce gli errori interni del server 
+app.use(notFound);                                      // Registrazione del middleware "notFound" che gestisce le rotte inesistenti (404 Not Found)
 
 
 /*********************
